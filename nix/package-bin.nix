5ce80455fe480761df5ef46f28caaf0db2de2af3
Kibeom Kim <503414+kkimdev@users.noreply.github.com>
2026-08-15 07:05:51 +0000

    fix(nix): 🐛 install the v0.2.3 release binary

--- nix/package-bin.nix
+++ nix/package-bin.nix
@@ -17,13 +17,13 @@ let
       or (throw "Unsupported system architecture: ${stdenv.hostPlatform.system}");
 
   hashMap = {
-    x86_64 = "sha256-fUjCvfoN5jvJZuDVndtFmQReD2DGOG+GQqund0/n9X0=";
-    aarch64 = "sha256-8okABAw9/UHoqXoIOgUpXK5uRCVUbn6ivGq+E9+hmus=";
+    x86_64 = "sha256-BO9DWHPvVDubujeRwGLN+cH0jk+N3wW6cLd2bO5cm7g=";
+    aarch64 = "sha256-5USw+y76Vo3KUflr1RmbwD0LV0s45iab1z3v7rkux6k=";
   };
 in
 stdenv.mkDerivation rec {
   pname = "sommelier-rs-bin";
-  version = "0.2.2";
+  version = "0.2.3";
 
   src = fetchurl {
     url = "https://github.com/kkimdev/sommelier-rs/releases/download/virtwl-v${version}/sommelier_rs_virtwl-v${version}-${targetCpu}";
